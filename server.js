a145a41bb87d38f197a9bb40256ba32e335268e4
CharlesABJ <117536741+CharlesABJ@users.noreply.github.com>
2023-05-12 22:51:17 +0200

    d

--- server.js
+++ server.js
@@ -1,48 +1,42 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
+const express = require("express");
+const path = require("path");
 
-const normalizePort = val => {
-	const port = parseInt(val, 10);
+const port = process.env.PORT || 5678;
+app.set("port", port);
 
-	if (isNaN(port)) {
-		return val;
-	}
-	if (port >= 0) {
-		return port;
-	}
-	return false;
-};
-const port = normalizePort(process.env.PORT ||'5678');
-app.set('port', port);
-
-const errorHandler = error => {
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
-	const address = server.address();
-	const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-	switch (error.code) {
-		case 'EACCES':
-			console.error(bind + ' requires elevated privileges.');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error(bind + ' is already in use.');
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-	const address = server.address();
-	const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-	console.log('Listening on ' + bind);
+// Définition du dossier "images" comme dossier statique
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
 server.listen(port);
-
